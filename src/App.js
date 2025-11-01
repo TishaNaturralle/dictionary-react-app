7060040481a7fdf55457d3880180108b61f57d3b
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2025-11-01 06:47:11 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <a href="#" className='btn btn-primary'>
+      Hello World
+      </a>
+    </div>
+  );
+}
+
+export default App;
