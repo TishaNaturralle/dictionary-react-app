3436bd6f3ccee0acc8dede9773a327ceefacde8b
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2025-11-01 06:55:55 +0200

    updated href root name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <a href="#" className='btn btn-primary'>
+      <a href="/" className='btn btn-primary'>
       Hello World
       </a>
     </div>
